afe120d71d9a55e929e5bda6792c8019fdede74a
Jesse Crocker <jesse@gaiagps.com>
2016-05-05 07:44:12 -0600

    add RDS log download script

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # AWS-Utilities
 Collection of scripts for interacting with Amazon Web Services
+
+
+## download-rds-logs.py
+Backup AWS RDS instance logs.
+
+Log files that have already been downloaded will not be re-downloaded.
+
+Options
+```
+Usage: download-rds-logs.py -i my-instance-id
+
+Options:
+  -h, --help            show this help message and exit
+  -d, --debug           Turn on debug logging
+  -q, --quiet           turn off all logging
+  -i INSTANCE, --instance=INSTANCE
+                        instance name
+  -o OUTPUT_DIR, --output=OUTPUT_DIR
+                        output directory
+  -r REGION, --region=REGION
+                        AWS region
+```
+
+Example usage and output: 
+```
+$ python download-rds-logs.py -i my-instance-id
+INFO:root:AdditionalDataPending:True Marker:14:438625
+INFO:root:requesting error/postgresql.log.2016-05-04-22 marker:14:438625 chunk:1
+INFO:root:AdditionalDataPending:True Marker:14:891049
+INFO:root:requesting error/postgresql.log.2016-05-04-22 marker:14:891049 chunk:2
+INFO:root:AdditionalDataPending:True Marker:14:1320763
+INFO:root:requesting error/postgresql.log.2016-05-04-22 marker:14:1320763 chunk:3
+INFO:root:AdditionalDataPending:True Marker:14:1758515
+INFO:root:requesting error/postgresql.log.2016-05-04-22 marker:14:1758515 chunk:4
+INFO:root:AdditionalDataPending:True Marker:14:2203827
+INFO:root:requesting error/postgresql.log.2016-05-04-22 marker:14:2203827 chunk:5
+INFO:root:AdditionalDataPending:True Marker:14:2649495
+INFO:root:requesting error/postgresql.log.2016-05-04-22 marker:14:2649495 chunk:6
+INFO:root:AdditionalDataPending:True Marker:14:3092815
+INFO:root:requesting error/postgresql.log.2016-05-04-22 marker:14:3092815 chunk:7
+INFO:root:AdditionalDataPending:False Marker:14:3532449
+```
---
+++ download-rds-logs.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import logging
+from optparse import OptionParser
+import os
+from boto import rds2
+import sys
+
+AWS_REGIONS = ["us-east-1", "us-west-1", "us-west-2", "eu-west-1", \
+    "eu-central-1", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1",\
+    "sa-east-1"]
+
+def _main():
+    usage = "usage: %prog -i my-instance-id"
+    parser = OptionParser(usage=usage,
+                          description="")
+    parser.add_option("-d", "--debug", action="store_true", dest="debug",
+                      help="Turn on debug logging")
+    parser.add_option("-q", "--quiet", action="store_true", dest="quiet",
+                      help="turn off all logging")
+    parser.add_option("-i", "--instance", action="store", dest="instance",
+                      default=None,
+                      help="instance name")
+    parser.add_option("-o", "--output", action="store", dest="output_dir",
+                      default="./",
+                      help="output directory")
+    parser.add_option("-r", "--region", action="store", dest="region",
+        default="us-east-1",
+        choices=AWS_REGIONS,
+        help="AWS region")
+    (options, args) = parser.parse_args()
+ 
+    logging.basicConfig(level=logging.DEBUG if options.debug else
+    (logging.ERROR if options.quiet else logging.INFO))
+
+    if not options.instance:
+        logging.error("Instance parameter is required")
+        sys.exit(-1)
+
+    if not os.path.exists(options.output_dir):
+        os.mkdir(options.output_dir)
+
+    connection = rds2.connect_to_region(options.region)
+    response = connection.describe_db_log_files(options.instance)
+    logfiles = response['DescribeDBLogFilesResponse']['DescribeDBLogFilesResult']['DescribeDBLogFiles']
+    for log in logfiles:
+        logging.debug(log)
+        logfilename = log['LogFileName']
+        destination = os.path.join(options.output_dir, os.path.basename(logfilename))
+
+        if os.path.exists(destination):
+            statinfo = os.stat(destination)
+            if statinfo.st_size == log['Size']:
+                logging.info("File %s exists, skipping" % (logfilename))
+                continue
+            else:
+                logging.info("Log file %s exists, but size does not match, redownloading." % (logfilename))
+                logging.info("Local files size %d expected size:%d" % (statinfo.st_size, log['Size']))
+                os.remove(destination)
+
+        chunk = 0
+        with open(destination, "w") as f:
+            more_data = True
+            marker = "0"
+            while more_data:
+                logging.info("requesting %s marker:%s chunk:%i" % (logfilename, marker, chunk))
+                response = connection.download_db_log_file_portion(options.instance, logfilename,
+                    marker=marker)
+                result = response['DownloadDBLogFilePortionResponse']['DownloadDBLogFilePortionResult']
+                logging.info("AdditionalDataPending:%s Marker:%s" % (str(result['AdditionalDataPending']), result['Marker']))
+                logging.debug(result)
+                if 'LogFileData' in result and result['LogFileData'] is not None:
+                    f.write(result['LogFileData'])
+                else:
+                    logging.error("No LogFileData for file:%s" % (logfilename))
+                more_data = 'AdditionalDataPending' in result and result['AdditionalDataPending']
+                if 'Marker' in result:
+                    marker = result['Marker']
+                chunk += 1
+
+if __name__ == "__main__":
+    _main()
